9d58fdc7904b39fa47c48604ae911d845e718807
Gbzzz <gbp3@aluno.ifal.edu.br>
2023-05-04 19:32:52 -0300

    add view_plans (navigation)

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,12 +18,10 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
-            </div>
 
-                <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('view_plans')" :active="request()->routeIs('view_plans')">
-                        {{ __('Ver planos') }}
+                        {{ __('Ver Planos') }}
                     </x-nav-link>
                 </div>
             </div>
