0c5866a7dccb6e66c2f1521debece7baaefe39cb
Gbzzz <gbp3@aluno.ifal.edu.br>
2023-05-04 19:58:02 -0300

    update view_plans

--- resources/views/plans/index.blade.php
+++ resources/views/plans/index.blade.php
@@ -9,8 +9,40 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Todos os planos slkewnjglikjgpiowegjWPOGPOGJAp") }}
-                </div>
+
+                    <div class="row justify-content-center align-items-center">
+                        <div class="col-md-4">
+                          <div class="card">
+                            <img class="card-img-top" src="imgs/maisMAguinho.png" width="75" height="65" alt="Card image cap">
+                            <div class="card-body">
+                              <h5 class="card-title">Plano Básico</h5>
+                              <p class="card-text">R$9,90/mês</p>
+                              <a href="#" class="btn btn-primary">Ver mais</a>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <div class="card">
+                            <img class="card-img-top" src="imgs/meiaBomba.png" width="75" height="65" alt="Card image cap">
+                            <div class="card-body">
+                              <h5 class="card-title">Plano Pro</h5>
+                              <p class="card-text">R$34,90/mês</p>
+                              <a href="#" class="btn btn-primary">Ver mais</a>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <div class="card">
+                            <img class="card-img-top" src="imgs/pesoBIG.png" width="75" height="65" alt="Card image cap">
+                            <div class="card-body">
+                              <h5 class="card-title">Plano Ultimate</h5>
+                              <p class="card-text">R$59,90/mês</p>
+                              <a href="#" class="btn btn-primary">Ver mais</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
             </div>
         </div>
     </div>
